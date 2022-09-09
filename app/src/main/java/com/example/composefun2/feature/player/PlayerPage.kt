9df8f64ceeba20e9cab958a7c376d9071f42044b
akvus <mr.akvus@gmail.com>
2022-09-09 17:23:08 +0200

    Split example2 big composable into children

--- app/src/main/java/com/example/composefun2/feature/player/PlayerPage.kt
+++ app/src/main/java/com/example/composefun2/feature/player/PlayerPage.kt
@@ -1,19 +1,24 @@
 package com.example.composefun2.feature.player
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.composefun2.LocalNavController
+import com.example.composefun2.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,7 +28,9 @@ fun PlayerPage() {
         topBar = { TopBar() },
         bottomBar = { BottomBar() }
     ) {
-        Text("Test")
+        Box(Modifier.padding(it)) {
+            PageBody()
+        }
     }
 }
 
@@ -64,7 +71,68 @@ private fun BottomBar() {
 @Composable
 private fun BottomNavIcon(icon: ImageVector, contentDescription: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(imageVector = icon, contentDescription = contentDescription)
+        Icon(icon, contentDescription = contentDescription)
         Text(contentDescription)
+    }
+}
+
+@Composable
+private fun PageBody() {
+    Column {
+        PlayListInfo()
+        ActionButtons()
+        SongsList()
+    }
+}
+
+@Composable
+private fun SongsList() {
+    LazyColumn {
+        item {
+            MusicFileListItem()
+        }
+    }
+}
+
+@Composable
+private fun ActionButtons() {
+    Row {
+        Button(onClick = { /*TODO*/ }) {
+            Text("Play")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text("Shuffle")
+        }
+    }
+}
+
+@Composable
+private fun PlayListInfo() {
+    val image: Painter = painterResource(id = R.drawable.birds2)
+
+    Row {
+        Image(
+            painter = image,
+            modifier = Modifier.size(120.dp),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = "Birds"
+        )
+        Column {
+            Text("Album * 10 songs * 2022")
+            Text("Charcoal")
+            Text("Chopin")
+            Row {
+                Icon(Icons.Outlined.Share, contentDescription = "Share")
+                Icon(Icons.Outlined.Share, contentDescription = "Share")
+                Icon(Icons.Outlined.Share, contentDescription = "Share")
+            }
+        }
+    }
+}
+
+@Composable
+private fun MusicFileListItem() {
+    Row {
+        Text("TODO")
     }
 }
