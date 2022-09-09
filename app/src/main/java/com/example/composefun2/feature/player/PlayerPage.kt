e3e89c9fca386d1c6a4b2d627f1ada18604087d5
akvus <mr.akvus@gmail.com>
2022-09-09 18:00:27 +0200

    Style bottom navigation

--- app/src/main/java/com/example/composefun2/feature/player/PlayerPage.kt
+++ app/src/main/java/com/example/composefun2/feature/player/PlayerPage.kt
@@ -2,21 +2,25 @@ package com.example.composefun2.feature.player
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composefun2.LocalNavController
 import com.example.composefun2.R
 
@@ -57,13 +61,19 @@ private fun TopBar() {
 
 @Composable
 private fun BottomBar() {
-    BottomAppBar {
-        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+    BottomAppBar(
+        containerColor = Color.Black
+    ) {
+        Row(
+            Modifier.fillMaxWidth()
+                .background(Color.White, shape = RoundedCornerShape(10.dp))
+                .padding(vertical = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
             BottomNavIcon(icon = Icons.Outlined.Home, contentDescription = "Home")
-            BottomNavIcon(icon = Icons.Outlined.Home, contentDescription = "Home")
-            BottomNavIcon(icon = Icons.Outlined.Home, contentDescription = "Home")
-            BottomNavIcon(icon = Icons.Outlined.Home, contentDescription = "Home")
-            BottomNavIcon(icon = Icons.Outlined.Home, contentDescription = "Home5")
+            BottomNavIcon(icon = Icons.Outlined.Search, contentDescription = "Search")
+            BottomNavIcon(icon = Icons.Outlined.List, contentDescription = "Library")
+            BottomNavIcon(icon = Icons.Outlined.Star, contentDescription = "Hotlist")
         }
     }
 }
@@ -72,7 +82,7 @@ private fun BottomBar() {
 private fun BottomNavIcon(icon: ImageVector, contentDescription: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Icon(icon, contentDescription = contentDescription)
-        Text(contentDescription)
+        Text(contentDescription, fontSize = 10.sp)
     }
 }
 
@@ -82,6 +92,7 @@ private fun PageBody() {
         PlayListInfo()
         ActionButtons()
         SongsList()
+        Spacer(Modifier.weight(1f))
         CurrentlyPlayedInfo()
     }
 }
@@ -134,10 +145,16 @@ private fun PlayListInfo() {
 @Composable
 private fun MusicFileListItem() {
     Row {
-        Text("TODO")
+        Text("Currently played")
     }
 }
 
-@Composable fun CurrentlyPlayedInfo() {
-    Text("Currently played")
+@Composable
+fun CurrentlyPlayedInfo() {
+    Row(
+        modifier = Modifier.fillMaxWidth().background(Color.Black).clip(RoundedCornerShape(10.dp))
+    ) {
+        Text("TODO")
+        Text("Currently played")
+    }
 }
