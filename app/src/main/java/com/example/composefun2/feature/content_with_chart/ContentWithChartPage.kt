ab9e9f581eefdb85510f0c8284b41b9d9dc32e6f
akvus <mr.akvus@gmail.com>
2022-09-09 22:57:30 +0200

    Add a skeleton to content with chart page

--- app/src/main/java/com/example/composefun2/feature/content_with_chart/ContentWithChartPage.kt
+++ app/src/main/java/com/example/composefun2/feature/content_with_chart/ContentWithChartPage.kt
@@ -1,10 +1,93 @@
 package com.example.composefun2.feature.content_with_chart
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.MailOutline
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.composefun2.LocalNavController
 
 // Based onhttps://lh3.googleusercontent.com/ejV1s6bC4cfMwhq38-bjOCNmFM0KoSfBjyW6dtUhkzFdg9JhiM7aTCj9qf4BDm0DdgY8igLCdweqmUo0lRg3DaYfceHNRy64RbGahlfjpDJtwUEW127kEUcTUQ9DgkK8LIP_7D3E
 
-@Composable fun ContentWithChartPage() {
-    Text(text = "Text")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContentWithChartPage() {
+    Scaffold(
+        topBar = {
+            TopBar()
+        }
+    ) {
+        Box(modifier = Modifier.padding(it)) {
+            PageBody()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TopBar() {
+    SmallTopAppBar(
+        title = {
+            Text("Content with a chart")
+        },
+        navigationIcon = {
+            val navController = LocalNavController.current
+            IconButton(onClick = {
+                navController.popBackStack()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Go back"
+                )
+            }
+        },
+        actions = {
+            Icon(
+                Icons.Outlined.MailOutline,
+                contentDescription = "Search",
+                modifier = Modifier.clickable { }.padding(16.dp)
+            )
+        }
+    )
+}
+
+@Composable
+private fun PageBody() {
+    Column {
+        LazyColumn(Modifier.weight(1f)) {
+            item {
+                PageHeader()
+                Chart()
+                PageContent()
+            }
+        }
+        Footer()
+    }
+}
+
+@Composable
+private fun PageHeader() {
+    Text("TODO")
+}
+
+@Composable
+private fun Chart() {
+    Text("TODO")
+}
+
+@Composable
+private fun PageContent() {
+    Text("TODO")
+}
+
+@Composable
+private fun Footer() {
+    Text("TODO")
 }
