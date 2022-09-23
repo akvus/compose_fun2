1f0acae6768dea54a30e35b7d4f47df621c5a477
akvus <mr.akvus@gmail.com>
2022-09-23 19:27:01 +0200

    Add Timber

--- app/src/main/java/com/example/composefun2/App.kt
+++ app/src/main/java/com/example/composefun2/App.kt
@@ -2,6 +2,11 @@ package com.example.composefun2
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    init {
+        Timber.plant(Timber.DebugTree())
+    }
+}
